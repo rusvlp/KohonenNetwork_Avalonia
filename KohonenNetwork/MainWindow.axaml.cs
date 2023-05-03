8ce79c3bb7c8949ad95aa5b0c4eda7a7c989cadc
rusvlp <qdrwlad@gmail.com>
2023-05-03 15:58:05 +0400

    initial

---
+++ KohonenNetwork/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+
+namespace KohonenNetwork;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void addGrid(int cols, int rows)
+    {
+        Grid grid = new Grid();
+        
+    }
+}
